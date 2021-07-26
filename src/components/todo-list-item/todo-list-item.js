03d96546b9df6231475cbe5f48cf711712c2026a
AlexMG28 <alex.m.gavrish@gmail.com>
2021-07-26 14:51:05 +0300

    add state & .done + .important

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -4,17 +4,53 @@ import './todo-list-item.css'
 
 export default class TodoListItem extends Component {
 
+    state = {
+        done: false,
+        important: false
+    }
+
+    onLabelClick = () => {
+        this.setState(( {done} )=>{
+            return {
+                done: !done
+            }
+        })
+            
+    }
+
+    onMarkImportant = () => {
+        this.setState(({ important }) => {
+            return {
+                important: !important
+            }
+        })
+    }
+
     render() {
         const { label } = this.props
+        const { done, important } = this.state
+
+        let classNames = 'todo-list-item'
+
+        if (done) {
+            classNames += ' done'
+        }
+
+        if (important) {
+            classNames += ' important'
+        }
+
         return (
-            <span className='d-flex justify-content-between'>
-                <span>
+            <span className={classNames}>
+                <span className='todo-list-item-label'
+                    onClick={this.onLabelClick}>
                     {label}
                 </span>
                 <div className='btn-group' role='group'>
                     <button
                         type='button'
-                        className='btn btn-outline-success'>
+                        className='btn btn-outline-success'
+                        onClick={this.onMarkImportant}>
                         Important
                     </button>
                     <button
